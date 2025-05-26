d5627f13aba31392ca56690dae3ec40e4417a351
x13 <jana19017@gmail.com>
2025-05-26 16:25:10 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Erorr.x13-Ip-Changer
-Ip-Changer Tool Changer Your Ip Every 5 Seconds
+# Anubis Ip Changer
+
+1 : git clone https://github.com/erorr-x13/Erorr.x13-Ip-Changer.git
+
+2 : cd -Anubis
+
+3 : python3 install.py
+
+4 : interminal type ( Anubis ) any where you want
+  
+5 : type time to change IP
+
+6 : type how many time to change your ip 
+
+*[0 to infinte IP change]
+
+7 : go to your browser / pc  change sock proxy to 127.0.0.1:9050
+
---
+++ install.py
@@ -0,0 +1,18 @@
+import os
+choice = input('[+] to install press (Y) to uninstall press (N) >> ')
+run = os.system
+if str(choice) =='Y' or str(choice)=='y':
+
+    run('chmod 777 Anubis.py')
+    run('mkdir /usr/share/Anubis')
+    run('cp Anubis.py /usr/share/Anubis/Anubis.py')
+
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/Anubis/Anubis.py "$@"')
+    with open('/usr/bin/Anubis','w')as file:
+        file.write(cmnd)
+    run('chmod +x /usr/bin/dbc & chmod +x /usr/share/Anubis/Anubis.py')
+    print('''\n\done install Anubis ip changer \nfrom now just type \x1b[6;30;42mdbc\x1b[0m in terminal ''')
+if str(choice)=='N' or str(choice)=='n':
+    run('rm -r /usr/share/Anubis ')
+    run('rm /usr/bin/Anubis ')
+    print('[!] Done install IP Changer Anubis')
