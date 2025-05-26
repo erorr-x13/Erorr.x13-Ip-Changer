2f9eb99576ef24948cf47ab46e3ab5f1f17ff508
x13 <jana19017@gmail.com>
2025-05-26 20:19:42 +0300

    install.py

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ if str(choice) =='Y' or str(choice)=='y':
     cmnd=(' #! /bin/sh \n exec python3 /usr/share/Anubis/Anubis.py "$@"')
     with open('/usr/bin/Anubis','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/dbc & chmod +x /usr/share/Anubis/Anubis.py')
+    run('chmod +x /usr/bin/Anubis & chmod +x /usr/share/Anubis/Anubis.py')
     print('''\n\done install Anubis ip changer \nfrom now just type \x1b[6;30;42mdbc\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/Anubis ')
